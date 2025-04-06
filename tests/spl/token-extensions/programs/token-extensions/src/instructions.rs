042d9d73957cb1460d9f5ac75a73e6f47314b8f2
CanardMandarin <thibault.marboud@gmail.com>
2025-04-06 08:42:03 +0000

    start immutable owner constraint

--- tests/spl/token-extensions/programs/token-extensions/src/instructions.rs
+++ tests/spl/token-extensions/programs/token-extensions/src/instructions.rs
@@ -68,6 +68,16 @@ pub struct CreateMintAccount<'info> {
         associated_token::authority = receiver,
     )]
     pub mint_token_account: Box<InterfaceAccount<'info, TokenAccount>>,
+    #[account(
+        init,
+        payer = payer,
+        token::token_program = token_program,
+        token::mint = mint,
+        token::authority = receiver,
+        extensions::immutable_owner,
+    )]
+    pub mint_immutable_token_account: Box<InterfaceAccount<'info, TokenAccount>>,
+
     /// CHECK: This account's data is a buffer of TLV data
     #[account(
         init,
@@ -189,6 +199,12 @@ pub struct CheckMintExtensionConstraints<'info> {
         extensions::pausable::authority = authority,
     )]
     pub mint: Box<InterfaceAccount<'info, Mint>>,
+    #[account(
+        token::mint = mint,
+        token::authority = authority,
+        extensions::immutable_owner,
+    )]
+    pub mint_immutable_token_account: Box<InterfaceAccount<'info, TokenAccount>>,
 }
 
 #[derive(Accounts)]
