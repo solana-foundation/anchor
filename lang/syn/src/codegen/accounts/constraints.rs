1fa136efd6c9d2e85dcdd5f1aec96f8a39a2edb3
CanardMandarin <thibault.marboud@gmail.com>
2025-04-06 08:45:44 +0000

    fmt

--- lang/syn/src/codegen/accounts/constraints.rs
+++ lang/syn/src/codegen/accounts/constraints.rs
@@ -703,7 +703,7 @@ fn generate_constraint_init_group(
                             };
                         }
 
-                                                
+
                         // Initialize the token account.
                         let cpi_program_id = #token_program.key();
                         let accounts = ::anchor_spl::token_interface::InitializeAccount3 {
@@ -1756,7 +1756,10 @@ impl<'a> OptionalCheckScope<'a> {
     }
 }
 
-fn generate_get_token_account_space(mint: &Expr, additional_extensions: &proc_macro2::TokenStream) -> proc_macro2::TokenStream {
+fn generate_get_token_account_space(
+    mint: &Expr,
+    additional_extensions: &proc_macro2::TokenStream,
+) -> proc_macro2::TokenStream {
     quote! {
         {
             let mint_info = #mint.to_account_info();
