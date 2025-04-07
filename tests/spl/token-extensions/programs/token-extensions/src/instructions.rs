13736f667420641240d67d6e1863342d6c90990a
CanardMandarin <thibault.marboud@gmail.com>
2025-04-07 04:44:58 +0000

    better tests

--- tests/spl/token-extensions/programs/token-extensions/src/instructions.rs
+++ tests/spl/token-extensions/programs/token-extensions/src/instructions.rs
@@ -63,9 +63,9 @@ pub struct CreateMintAccount<'info> {
     #[account(
         init,
         payer = payer,
-        associated_token::token_program = token_program,
-        associated_token::mint = mint,
-        associated_token::authority = receiver,
+        token::token_program = token_program,
+        token::mint = mint,
+        token::authority = receiver,
     )]
     pub mint_token_account: Box<InterfaceAccount<'info, TokenAccount>>,
     #[account(
@@ -198,6 +198,13 @@ pub struct CheckMintExtensionConstraints<'info> {
         extensions::permanent_delegate::delegate = authority,
         extensions::pausable::authority = authority,
     )]
+    pub mint: Box<InterfaceAccount<'info, Mint>>,
+}
+
+#[derive(Accounts)]
+#[instruction()]
+pub struct CheckTokenAccountExtensionConstraints<'info> {
+    pub authority: Signer<'info>,
     pub mint: Box<InterfaceAccount<'info, Mint>>,
     #[account(
         token::mint = mint,
@@ -407,4 +414,17 @@ pub fn update_and_remove_token_metadata_handler(
     let metadata = get_mint_extensible_extension_data::<TokenMetadata>(mint_data)?;
     assert_eq!(metadata.additional_metadata.len(), 0);
     Ok(())
+}
+
+#[derive(Accounts)]
+#[instruction()]
+pub struct CheckMissingTokenAccountExtensionConstraints<'info> {
+    pub authority: Signer<'info>,
+    pub mint: Box<InterfaceAccount<'info, Mint>>,
+    #[account(
+        token::mint = mint,
+        token::authority = authority,
+        extensions::immutable_owner,
+    )]
+    pub mint_token_account: Box<InterfaceAccount<'info, TokenAccount>>,
 }
