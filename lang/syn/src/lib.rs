925fa8a2738483efc3bff051867deff707da9c4e
rute-sophie <rute.c.lemos@gmail.com>
2025-11-27 00:58:25 +0000

    cargo fmt

--- lang/syn/src/lib.rs
+++ lang/syn/src/lib.rs
@@ -834,7 +834,7 @@ pub enum ConstraintToken {
     ExtensionTokenHookAuthority(Context<ConstraintExtensionAuthority>),
     ExtensionTokenHookProgramId(Context<ConstraintExtensionTokenHookProgramId>),
     ExtensionPermanentDelegate(Context<ConstraintExtensionPermanentDelegate>),
-    ExtensionPausableAuthority(Context<ConstraintExtensionAuthority>)
+    ExtensionPausableAuthority(Context<ConstraintExtensionAuthority>),
 }
 
 impl Parse for ConstraintToken {
