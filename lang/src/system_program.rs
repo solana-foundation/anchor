d8f228b6c7c23bdb802f96c858da2826d6b11742
Aursen <aursen@users.noreply.github.com>
2025-12-18 16:01:12 +0700

    fix system program

--- lang/src/system_program.rs
+++ lang/src/system_program.rs
@@ -24,20 +24,39 @@ pub fn advance_nonce_account<'info>(
     crate::solana_program::program::invoke_signed(
         &ix,
         &[
-            ctx.accounts.nonce.to_account_info(),
-            ctx.accounts.recent_blockhashes.to_account_info(),
-            ctx.accounts.authorized.to_account_info(),
+            ctx.accounts.nonce,
+            ctx.accounts.recent_blockhashes,
+            ctx.accounts.authorized,
         ],
         ctx.signer_seeds,
     )
     .map_err(Into::into)
 }
 
-#[derive(Accounts)]
 pub struct AdvanceNonceAccount<'info> {
-    pub nonce: UncheckedAccount<'info>,
-    pub authorized: UncheckedAccount<'info>,
-    pub recent_blockhashes: UncheckedAccount<'info>,
+    pub nonce: AccountInfo<'info>,
+    pub authorized: AccountInfo<'info>,
+    pub recent_blockhashes: AccountInfo<'info>,
+}
+
+impl<'info> ToAccountInfos<'info> for AdvanceNonceAccount<'info> {
+    fn to_account_infos(&self) -> Vec<AccountInfo<'info>> {
+        vec![
+            self.nonce.to_owned(),
+            self.recent_blockhashes.to_owned(),
+            self.authorized.to_owned(),
+        ]
+    }
+}
+
+impl<'info> ToAccountMetas for AdvanceNonceAccount<'info> {
+    fn to_account_metas(&self, _: Option<bool>) -> Vec<AccountMeta> {
+        vec![
+            AccountMeta::new(self.nonce.key(), false),
+            AccountMeta::new_readonly(self.recent_blockhashes.key(), false),
+            AccountMeta::new_readonly(self.authorized.key(), true),
+        ]
+    }
 }
 
 pub fn allocate<'info>(
@@ -50,15 +69,26 @@ pub fn allocate<'info>(
     );
     crate::solana_program::program::invoke_signed(
         &ix,
-        &[ctx.accounts.account_to_allocate.to_account_info()],
+        &[ctx.accounts.account_to_allocate],
         ctx.signer_seeds,
     )
     .map_err(Into::into)
 }
 
-#[derive(Accounts)]
 pub struct Allocate<'info> {
-    pub account_to_allocate: UncheckedAccount<'info>,
+    pub account_to_allocate: AccountInfo<'info>,
+}
+
+impl<'info> ToAccountInfos<'info> for Allocate<'info> {
+    fn to_account_infos(&self) -> Vec<AccountInfo<'info>> {
+        vec![self.account_to_allocate.to_owned()]
+    }
+}
+
+impl<'info> ToAccountMetas for Allocate<'info> {
+    fn to_account_metas(&self, _: Option<bool>) -> Vec<AccountMeta> {
+        vec![AccountMeta::new(self.account_to_allocate.key(), true)]
+    }
 }
 
 pub fn allocate_with_seed<'info>(
@@ -76,19 +106,30 @@ pub fn allocate_with_seed<'info>(
     );
     crate::solana_program::program::invoke_signed(
         &ix,
-        &[
-            ctx.accounts.account_to_allocate.to_account_info(),
-            ctx.accounts.base.to_account_info(),
-        ],
+        &[ctx.accounts.account_to_allocate, ctx.accounts.base],
         ctx.signer_seeds,
     )
     .map_err(Into::into)
 }
 
-#[derive(Accounts)]
 pub struct AllocateWithSeed<'info> {
-    pub account_to_allocate: UncheckedAccount<'info>,
-    pub base: UncheckedAccount<'info>,
+    pub account_to_allocate: AccountInfo<'info>,
+    pub base: AccountInfo<'info>,
+}
+
+impl<'info> ToAccountInfos<'info> for AllocateWithSeed<'info> {
+    fn to_account_infos(&self) -> Vec<AccountInfo<'info>> {
+        vec![self.account_to_allocate.to_owned(), self.base.to_owned()]
+    }
+}
+
+impl<'info> ToAccountMetas for AllocateWithSeed<'info> {
+    fn to_account_metas(&self, _: Option<bool>) -> Vec<AccountMeta> {
+        vec![
+            AccountMeta::new(self.account_to_allocate.key(), false),
+            AccountMeta::new_readonly(self.base.key(), true),
+        ]
+    }
 }
 
 pub fn assign<'info>(
@@ -101,15 +142,26 @@ pub fn assign<'info>(
     );
     crate::solana_program::program::invoke_signed(
         &ix,
-        &[ctx.accounts.account_to_assign.to_account_info()],
+        &[ctx.accounts.account_to_assign],
         ctx.signer_seeds,
     )
     .map_err(Into::into)
 }
 
-#[derive(Accounts)]
 pub struct Assign<'info> {
-    pub account_to_assign: UncheckedAccount<'info>,
+    pub account_to_assign: AccountInfo<'info>,
+}
+
+impl<'info> ToAccountInfos<'info> for Assign<'info> {
+    fn to_account_infos(&self) -> Vec<AccountInfo<'info>> {
+        vec![self.account_to_assign.to_owned()]
+    }
+}
+
+impl<'info> ToAccountMetas for Assign<'info> {
+    fn to_account_metas(&self, _: Option<bool>) -> Vec<AccountMeta> {
+        vec![AccountMeta::new(self.account_to_assign.key(), true)]
+    }
 }
 
 pub fn assign_with_seed<'info>(
@@ -125,19 +177,30 @@ pub fn assign_with_seed<'info>(
     );
     crate::solana_program::program::invoke_signed(
         &ix,
-        &[
-            ctx.accounts.account_to_assign.to_account_info(),
-            ctx.accounts.base.to_account_info(),
-        ],
+        &[ctx.accounts.account_to_assign, ctx.accounts.base],
         ctx.signer_seeds,
     )
     .map_err(Into::into)
 }
 
-#[derive(Accounts)]
 pub struct AssignWithSeed<'info> {
-    pub account_to_assign: UncheckedAccount<'info>,
-    pub base: UncheckedAccount<'info>,
+    pub account_to_assign: AccountInfo<'info>,
+    pub base: AccountInfo<'info>,
+}
+
+impl<'info> ToAccountInfos<'info> for AssignWithSeed<'info> {
+    fn to_account_infos(&self) -> Vec<AccountInfo<'info>> {
+        vec![self.account_to_assign.to_owned(), self.base.to_owned()]
+    }
+}
+
+impl<'info> ToAccountMetas for AssignWithSeed<'info> {
+    fn to_account_metas(&self, _: Option<bool>) -> Vec<AccountMeta> {
+        vec![
+            AccountMeta::new(self.account_to_assign.key(), false),
+            AccountMeta::new_readonly(self.base.key(), true),
+        ]
+    }
 }
 
 pub fn authorize_nonce_account<'info>(
@@ -151,19 +214,30 @@ pub fn authorize_nonce_account<'info>(
     );
     crate::solana_program::program::invoke_signed(
         &ix,
-        &[
-            ctx.accounts.nonce.to_account_info(),
-            ctx.accounts.authorized.to_account_info(),
-        ],
+        &[ctx.accounts.nonce, ctx.accounts.authorized],
         ctx.signer_seeds,
     )
     .map_err(Into::into)
 }
 
-#[derive(Accounts)]
 pub struct AuthorizeNonceAccount<'info> {
-    pub nonce: UncheckedAccount<'info>,
-    pub authorized: UncheckedAccount<'info>,
+    pub nonce: AccountInfo<'info>,
+    pub authorized: AccountInfo<'info>,
+}
+
+impl<'info> ToAccountInfos<'info> for AuthorizeNonceAccount<'info> {
+    fn to_account_infos(&self) -> Vec<AccountInfo<'info>> {
+        vec![self.nonce.to_owned(), self.authorized.to_owned()]
+    }
+}
+
+impl<'info> ToAccountMetas for AuthorizeNonceAccount<'info> {
+    fn to_account_metas(&self, _: Option<bool>) -> Vec<AccountMeta> {
+        vec![
+            AccountMeta::new(self.nonce.key(), false),
+            AccountMeta::new_readonly(self.authorized.key(), true),
+        ]
+    }
 }
 
 pub fn create_account<'info>(
@@ -181,19 +255,30 @@ pub fn create_account<'info>(
     );
     crate::solana_program::program::invoke_signed(
         &ix,
-        &[
-            ctx.accounts.from.to_account_info(),
-            ctx.accounts.to.to_account_info(),
-        ],
+        &[ctx.accounts.from, ctx.accounts.to],
         ctx.signer_seeds,
     )
     .map_err(Into::into)
 }
 
-#[derive(Accounts)]
 pub struct CreateAccount<'info> {
-    pub from: UncheckedAccount<'info>,
-    pub to: UncheckedAccount<'info>,
+    pub from: AccountInfo<'info>,
+    pub to: AccountInfo<'info>,
+}
+
+impl<'info> ToAccountInfos<'info> for CreateAccount<'info> {
+    fn to_account_infos(&self) -> Vec<AccountInfo<'info>> {
+        vec![self.from.to_owned(), self.to.to_owned()]
+    }
+}
+
+impl<'info> ToAccountMetas for CreateAccount<'info> {
+    fn to_account_metas(&self, _: Option<bool>) -> Vec<AccountMeta> {
+        vec![
+            AccountMeta::new(self.from.key(), true),
+            AccountMeta::new(self.to.key(), true),
+        ]
+    }
 }
 
 pub fn create_account_with_seed<'info>(
@@ -214,21 +299,36 @@ pub fn create_account_with_seed<'info>(
     );
     crate::solana_program::program::invoke_signed(
         &ix,
-        &[
-            ctx.accounts.from.to_account_info(),
-            ctx.accounts.to.to_account_info(),
-            ctx.accounts.base.to_account_info(),
-        ],
+        &[ctx.accounts.from, ctx.accounts.to, ctx.accounts.base],
         ctx.signer_seeds,
     )
     .map_err(Into::into)
 }
 
-#[derive(Accounts)]
 pub struct CreateAccountWithSeed<'info> {
-    pub from: UncheckedAccount<'info>,
-    pub to: UncheckedAccount<'info>,
-    pub base: UncheckedAccount<'info>,
+    pub from: AccountInfo<'info>,
+    pub to: AccountInfo<'info>,
+    pub base: AccountInfo<'info>,
+}
+
+impl<'info> ToAccountInfos<'info> for CreateAccountWithSeed<'info> {
+    fn to_account_infos(&self) -> Vec<AccountInfo<'info>> {
+        vec![
+            self.from.to_owned(),
+            self.to.to_owned(),
+            self.base.to_owned(),
+        ]
+    }
+}
+
+impl<'info> ToAccountMetas for CreateAccountWithSeed<'info> {
+    fn to_account_metas(&self, _: Option<bool>) -> Vec<AccountMeta> {
+        vec![
+            AccountMeta::new(self.from.key(), true),
+            AccountMeta::new(self.to.key(), false),
+            AccountMeta::new_readonly(self.to.key(), true),
+        ]
+    }
 }
 
 pub fn create_nonce_account<'info>(
@@ -244,31 +344,49 @@ pub fn create_nonce_account<'info>(
     );
     crate::solana_program::program::invoke_signed(
         &ixs[0],
-        &[
-            ctx.accounts.from.to_account_info(),
-            ctx.accounts.nonce.to_account_info(),
-        ],
+        &[ctx.accounts.from, ctx.accounts.nonce.clone()],
         ctx.signer_seeds,
     )?;
 
     crate::solana_program::program::invoke_signed(
         &ixs[1],
         &[
-            ctx.accounts.nonce.to_account_info(),
-            ctx.accounts.recent_blockhashes.to_account_info(),
-            ctx.accounts.rent.to_account_info(),
+            ctx.accounts.nonce,
+            ctx.accounts.recent_blockhashes,
+            ctx.accounts.rent,
         ],
         ctx.signer_seeds,
     )
     .map_err(Into::into)
 }
 
-#[derive(Accounts)]
 pub struct CreateNonceAccount<'info> {
-    pub from: UncheckedAccount<'info>,
-    pub nonce: UncheckedAccount<'info>,
-    pub recent_blockhashes: UncheckedAccount<'info>,
-    pub rent: UncheckedAccount<'info>,
+    pub from: AccountInfo<'info>,
+    pub nonce: AccountInfo<'info>,
+    pub recent_blockhashes: AccountInfo<'info>,
+    pub rent: AccountInfo<'info>,
+}
+
+impl<'info> ToAccountInfos<'info> for CreateNonceAccount<'info> {
+    fn to_account_infos(&self) -> Vec<AccountInfo<'info>> {
+        vec![
+            self.from.to_owned(),
+            self.nonce.to_owned(),
+            self.recent_blockhashes.to_owned(),
+            self.rent.to_owned(),
+        ]
+    }
+}
+
+impl<'info> ToAccountMetas for CreateNonceAccount<'info> {
+    fn to_account_metas(&self, _: Option<bool>) -> Vec<AccountMeta> {
+        vec![
+            AccountMeta::new(self.from.key(), true),
+            AccountMeta::new(self.nonce.key(), false),
+            AccountMeta::new_readonly(self.recent_blockhashes.key(), false),
+            AccountMeta::new_readonly(self.rent.key(), false),
+        ]
+    }
 }
 
 pub fn create_nonce_account_with_seed<'info>(
@@ -288,9 +406,9 @@ pub fn create_nonce_account_with_seed<'info>(
     crate::solana_program::program::invoke_signed(
         &ixs[0],
         &[
-            ctx.accounts.from.to_account_info(),
-            ctx.accounts.nonce.to_account_info(),
-            ctx.accounts.base.to_account_info(),
+            ctx.accounts.from,
+            ctx.accounts.nonce.clone(),
+            ctx.accounts.base,
         ],
         ctx.signer_seeds,
     )?;
@@ -298,22 +416,45 @@ pub fn create_nonce_account_with_seed<'info>(
     crate::solana_program::program::invoke_signed(
         &ixs[1],
         &[
-            ctx.accounts.nonce.to_account_info(),
-            ctx.accounts.recent_blockhashes.to_account_info(),
-            ctx.accounts.rent.to_account_info(),
+            ctx.accounts.nonce,
+            ctx.accounts.recent_blockhashes,
+            ctx.accounts.rent,
         ],
         ctx.signer_seeds,
     )
     .map_err(Into::into)
 }
 
-#[derive(Accounts)]
 pub struct CreateNonceAccountWithSeed<'info> {
-    pub from: UncheckedAccount<'info>,
-    pub nonce: UncheckedAccount<'info>,
-    pub base: UncheckedAccount<'info>,
-    pub recent_blockhashes: UncheckedAccount<'info>,
-    pub rent: UncheckedAccount<'info>,
+    pub from: AccountInfo<'info>,
+    pub nonce: AccountInfo<'info>,
+    pub base: AccountInfo<'info>,
+    pub recent_blockhashes: AccountInfo<'info>,
+    pub rent: AccountInfo<'info>,
+}
+
+impl<'info> ToAccountInfos<'info> for CreateNonceAccountWithSeed<'info> {
+    fn to_account_infos(&self) -> Vec<AccountInfo<'info>> {
+        vec![
+            self.from.to_owned(),
+            self.nonce.to_owned(),
+            self.base.to_owned(),
+            self.recent_blockhashes.to_owned(),
+            self.rent.to_owned(),
+        ]
+    }
+}
+
+impl<'info> ToAccountMetas for CreateNonceAccountWithSeed<'info> {
+    fn to_account_metas(&self, _: Option<bool>) -> Vec<AccountMeta> {
+        vec![
+            AccountMeta::new(self.from.key(), false),
+            AccountMeta::new(self.nonce.key(), false),
+            AccountMeta::new_readonly(self.base.key(), true),
+            AccountMeta::new_readonly(self.recent_blockhashes.key(), false),
+            AccountMeta::new_readonly(self.rent.key(), false),
+        ]
+    }
 }
 
 pub fn transfer<'info>(
@@ -327,19 +468,30 @@ pub fn transfer<'info>(
     );
     crate::solana_program::program::invoke_signed(
         &ix,
-        &[
-            ctx.accounts.from.to_account_info(),
-            ctx.accounts.to.to_account_info(),
-        ],
+        &[ctx.accounts.from, ctx.accounts.to],
         ctx.signer_seeds,
     )
     .map_err(Into::into)
 }
 
-#[derive(Accounts)]
 pub struct Transfer<'info> {
-    pub from: UncheckedAccount<'info>,
-    pub to: UncheckedAccount<'info>,
+    pub from: AccountInfo<'info>,
+    pub to: AccountInfo<'info>,
+}
+
+impl<'info> ToAccountInfos<'info> for Transfer<'info> {
+    fn to_account_infos(&self) -> Vec<AccountInfo<'info>> {
+        vec![self.from.to_owned(), self.to.to_owned()]
+    }
+}
+
+impl<'info> ToAccountMetas for Transfer<'info> {
+    fn to_account_metas(&self, _: Option<bool>) -> Vec<AccountMeta> {
+        vec![
+            AccountMeta::new(self.from.key(), true),
+            AccountMeta::new(self.to.key(), false),
+        ]
+    }
 }
 
 pub fn transfer_with_seed<'info>(
@@ -358,21 +510,36 @@ pub fn transfer_with_seed<'info>(
     );
     crate::solana_program::program::invoke_signed(
         &ix,
-        &[
-            ctx.accounts.from.to_account_info(),
-            ctx.accounts.base.to_account_info(),
-            ctx.accounts.to.to_account_info(),
-        ],
+        &[ctx.accounts.from, ctx.accounts.base, ctx.accounts.to],
         ctx.signer_seeds,
     )
     .map_err(Into::into)
 }
 
-#[derive(Accounts)]
 pub struct TransferWithSeed<'info> {
-    pub from: UncheckedAccount<'info>,
-    pub base: UncheckedAccount<'info>,
-    pub to: UncheckedAccount<'info>,
+    pub from: AccountInfo<'info>,
+    pub base: AccountInfo<'info>,
+    pub to: AccountInfo<'info>,
+}
+
+impl<'info> ToAccountInfos<'info> for TransferWithSeed<'info> {
+    fn to_account_infos(&self) -> Vec<AccountInfo<'info>> {
+        vec![
+            self.from.to_owned(),
+            self.base.to_owned(),
+            self.to.to_owned(),
+        ]
+    }
+}
+
+impl<'info> ToAccountMetas for TransferWithSeed<'info> {
+    fn to_account_metas(&self, _: Option<bool>) -> Vec<AccountMeta> {
+        vec![
+            AccountMeta::new(self.from.key(), false),
+            AccountMeta::new_readonly(self.base.key(), true),
+            AccountMeta::new(self.to.key(), false),
+        ]
+    }
 }
 
 pub fn withdraw_nonce_account<'info>(
@@ -388,22 +555,45 @@ pub fn withdraw_nonce_account<'info>(
     crate::solana_program::program::invoke_signed(
         &ix,
         &[
-            ctx.accounts.nonce.to_account_info(),
-            ctx.accounts.to.to_account_info(),
-            ctx.accounts.recent_blockhashes.to_account_info(),
-            ctx.accounts.rent.to_account_info(),
-            ctx.accounts.authorized.to_account_info(),
+            ctx.accounts.nonce,
+            ctx.accounts.to,
+            ctx.accounts.recent_blockhashes,
+            ctx.accounts.rent,
+            ctx.accounts.authorized,
         ],
         ctx.signer_seeds,
     )
     .map_err(Into::into)
 }
 
-#[derive(Accounts)]
 pub struct WithdrawNonceAccount<'info> {
-    pub nonce: UncheckedAccount<'info>,
-    pub to: UncheckedAccount<'info>,
-    pub recent_blockhashes: UncheckedAccount<'info>,
-    pub rent: UncheckedAccount<'info>,
-    pub authorized: UncheckedAccount<'info>,
+    pub nonce: AccountInfo<'info>,
+    pub to: AccountInfo<'info>,
+    pub recent_blockhashes: AccountInfo<'info>,
+    pub rent: AccountInfo<'info>,
+    pub authorized: AccountInfo<'info>,
+}
+
+impl<'info> ToAccountInfos<'info> for WithdrawNonceAccount<'info> {
+    fn to_account_infos(&self) -> Vec<AccountInfo<'info>> {
+        vec![
+            self.nonce.to_owned(),
+            self.to.to_owned(),
+            self.recent_blockhashes.to_owned(),
+            self.rent.to_owned(),
+            self.authorized.to_owned(),
+        ]
+    }
+}
+
+impl<'info> ToAccountMetas for WithdrawNonceAccount<'info> {
+    fn to_account_metas(&self, _: Option<bool>) -> Vec<AccountMeta> {
+        vec![
+            AccountMeta::new(self.nonce.key(), false),
+            AccountMeta::new(self.to.key(), false),
+            AccountMeta::new_readonly(self.recent_blockhashes.key(), false),
+            AccountMeta::new_readonly(self.rent.key(), false),
+            AccountMeta::new_readonly(self.authorized.key(), true),
+        ]
+    }
 }
