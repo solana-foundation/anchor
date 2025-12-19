353fc6546f38a39f59116deb6c1bace30d378371
Akash Thota <Otter-0x4ka5h@osec.io>
2025-12-19 18:52:25 +0530

    fix: spl account packing

--- spl/src/token_interface.rs
+++ spl/src/token_interface.rs
@@ -1,37 +1,25 @@
 use anchor_lang::__private::bytemuck::Pod;
-use anchor_lang::pinocchio_runtime::program_pack::Pack;
-use anchor_lang::pinocchio_runtime::pubkey::Pubkey;
 use anchor_lang::pinocchio_runtime::account_info::AccountInfo;
-use pinocchio_token_2022::extension::ExtensionType;
-use pinocchio_token_2022::extension::{BaseStateWithExtensions, Extension, StateWithExtensions};
+use anchor_lang::pinocchio_runtime::pubkey::Pubkey;
 use std::ops::Deref;
+
+use spl_token_2022_interface::extension::{Extension, ExtensionType};
 
 pub use crate::token_2022::*;
 #[cfg(feature = "token_2022_extensions")]
 pub use crate::token_2022_extensions::*;
-use {
-    anchor_lang::{
-        __private::bytemuck::Pod,
-        solana_program::{program_pack::Pack, pubkey::Pubkey},
-    },
-    spl_token_2022::extension::{
-        BaseStateWithExtensions, Extension, ExtensionType, StateWithExtensions,
-    },
-    std::ops::Deref,
-};
 
 static IDS: [Pubkey; 2] = [pinocchio_token::ID, pinocchio_token_2022::ID];
 
-#[derive(Clone, Debug, Default, PartialEq)]
+#[derive()]
 pub struct TokenAccount(pinocchio_token_2022::state::TokenAccount);
 
 impl anchor_lang::AccountDeserialize for TokenAccount {
     fn try_deserialize_unchecked(buf: &mut &[u8]) -> anchor_lang::Result<Self> {
-        pinocchio_token_2022::extension::StateWithExtensions::<pinocchio_token_2022::state::TokenAccount>::unpack(
-            buf,
-        )
-        .map(|t| TokenAccount(t.base))
-        .map_err(Into::into)
+        let token_account_ref =
+            unsafe { pinocchio_token_2022::state::TokenAccount::from_bytes_unchecked(buf) };
+        let token_account = unsafe { std::ptr::read(token_account_ref) };
+        Ok(TokenAccount(token_account))
     }
 }
 
@@ -51,14 +39,14 @@ impl Deref for TokenAccount {
     }
 }
 
-#[derive(Clone, Debug, Default, PartialEq)]
+#[derive()]
 pub struct Mint(pinocchio_token_2022::state::Mint);
 
 impl anchor_lang::AccountDeserialize for Mint {
     fn try_deserialize_unchecked(buf: &mut &[u8]) -> anchor_lang::Result<Self> {
-        pinocchio_token_2022::extension::StateWithExtensions::<pinocchio_token_2022::state::Mint>::unpack(buf)
-            .map(|t| Mint(t.base))
-            .map_err(Into::into)
+        let mint_ref = unsafe { pinocchio_token_2022::state::Mint::from_bytes_unchecked(buf) };
+        let mint = unsafe { std::ptr::read(mint_ref) };
+        Ok(Mint(mint))
     }
 }
 
@@ -91,20 +79,17 @@ pub type ExtensionsVec = Vec<ExtensionType>;
 
 pub fn find_mint_account_size(extensions: Option<&ExtensionsVec>) -> anchor_lang::Result<usize> {
     if let Some(extensions) = extensions {
-        Ok(ExtensionType::try_calculate_account_len::<
-            pinocchio_token_2022::state::Mint,
-        >(extensions)?)
+        Ok(todo!())
     } else {
-        Ok(pinocchio_token_2022::state::Mint::LEN)
+        Ok(todo!())
     }
 }
 
 pub fn get_mint_extension_data<T: Extension + Pod>(
     account: &AccountInfo,
 ) -> anchor_lang::Result<T> {
-    let mint_data = account.data.borrow();
-    let mint_with_extension =
-        StateWithExtensions::<pinocchio_token_2022::state::Mint>::unpack(&mint_data)?;
+    let mint_data = unsafe { account.borrow_unchecked() };
+    let mint_with_extension = todo!();
     let extension_data = *mint_with_extension.get_extension::<T>()?;
     Ok(extension_data)
 }
