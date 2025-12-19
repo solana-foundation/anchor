fa921c05160f95809c4bf90074eea7b83adef64e
Akash Thota <Otter-0x4ka5h@osec.io>
2025-12-19 18:10:52 +0530

    token-22 extensions replaces with todo!()

--- spl/src/token_2022_extensions/mint_close_authority.rs
+++ spl/src/token_2022_extensions/mint_close_authority.rs
@@ -1,30 +1,20 @@
 // Avoiding AccountInfo deprecated msg in anchor context
 #![allow(deprecated)]
-use anchor_lang::solana_program::account_info::AccountInfo;
-use anchor_lang::solana_program::pubkey::Pubkey;
+use anchor_lang::pinocchio_runtime::account_info::AccountInfo;
+use anchor_lang::pinocchio_runtime::pubkey::Pubkey;
 use anchor_lang::Result;
 use anchor_lang::{context::CpiContext, Accounts};
-use spl_token_2022_interface as spl_token_2022;
 
-pub fn mint_close_authority_initialize<'info>(
-    ctx: CpiContext<'_, '_, '_, 'info, MintCloseAuthorityInitialize<'info>>,
+pub fn mint_close_authority_initialize(
+    ctx: CpiContext<'_, '_, 'static, MintCloseAuthorityInitialize>,
     authority: Option<&Pubkey>,
 ) -> Result<()> {
-    let ix = spl_token_2022::instruction::initialize_mint_close_authority(
-        ctx.accounts.token_program_id.key,
-        ctx.accounts.mint.key,
-        authority,
-    )?;
-    anchor_lang::solana_program::program::invoke_signed(
-        &ix,
-        &[ctx.accounts.token_program_id, ctx.accounts.mint],
-        ctx.signer_seeds,
-    )
-    .map_err(Into::into)
+    let ix = todo!();
+    ix.invoke_signed(ctx.signer_seeds).map_err(Into::into)
 }
 
 #[derive(Accounts)]
-pub struct MintCloseAuthorityInitialize<'info> {
-    pub token_program_id: AccountInfo<'info>,
-    pub mint: AccountInfo<'info>,
+pub struct MintCloseAuthorityInitialize {
+    pub token_program_id: AccountInfo,
+    pub mint: AccountInfo,
 }
--- spl/src/token_2022_extensions/token_metadata.rs
+++ spl/src/token_2022_extensions/token_metadata.rs
@@ -1,109 +1,60 @@
 // Avoiding AccountInfo deprecated msg in anchor context
 #![allow(deprecated)]
-use anchor_lang::solana_program::account_info::AccountInfo;
-use anchor_lang::solana_program::pubkey::Pubkey;
+use anchor_lang::pinocchio_runtime::account_info::AccountInfo;
+use anchor_lang::pinocchio_runtime::pubkey::Pubkey;
 use anchor_lang::Result;
 use anchor_lang::{context::CpiContext, Accounts};
 
 use spl_pod::optional_keys::OptionalNonZeroPubkey;
 use spl_token_metadata_interface::state::Field;
 
-pub fn token_metadata_initialize<'info>(
-    ctx: CpiContext<'_, '_, '_, 'info, TokenMetadataInitialize<'info>>,
+pub fn token_metadata_initialize(
+    ctx: CpiContext<'_, '_, 'static, TokenMetadataInitialize>,
     name: String,
     symbol: String,
     uri: String,
 ) -> Result<()> {
-    let ix = spl_token_metadata_interface::instruction::initialize(
-        ctx.accounts.program_id.key,
-        ctx.accounts.metadata.key,
-        ctx.accounts.update_authority.key,
-        ctx.accounts.mint.key,
-        ctx.accounts.mint_authority.key,
-        name,
-        symbol,
-        uri,
-    );
-    anchor_lang::solana_program::program::invoke_signed(
-        &ix,
-        &[
-            ctx.accounts.program_id,
-            ctx.accounts.metadata,
-            ctx.accounts.update_authority,
-            ctx.accounts.mint,
-            ctx.accounts.mint_authority,
-        ],
-        ctx.signer_seeds,
-    )
-    .map_err(Into::into)
+    let ix = todo!();
+    ix.invoke_signed(ctx.signer_seeds).map_err(Into::into)
 }
 
 #[derive(Accounts)]
-pub struct TokenMetadataInitialize<'info> {
-    pub program_id: AccountInfo<'info>,
-    pub metadata: AccountInfo<'info>,
-    pub update_authority: AccountInfo<'info>,
-    pub mint_authority: AccountInfo<'info>,
-    pub mint: AccountInfo<'info>,
+pub struct TokenMetadataInitialize {
+    pub program_id: AccountInfo,
+    pub metadata: AccountInfo,
+    pub update_authority: AccountInfo,
+    pub mint_authority: AccountInfo,
+    pub mint: AccountInfo,
 }
 
-pub fn token_metadata_update_authority<'info>(
-    ctx: CpiContext<'_, '_, '_, 'info, TokenMetadataUpdateAuthority<'info>>,
+pub fn token_metadata_update_authority(
+    ctx: CpiContext<'_, '_, 'static, TokenMetadataUpdateAuthority>,
     new_authority: OptionalNonZeroPubkey,
 ) -> Result<()> {
-    let ix = spl_token_metadata_interface::instruction::update_authority(
-        ctx.accounts.program_id.key,
-        ctx.accounts.metadata.key,
-        ctx.accounts.current_authority.key,
-        new_authority,
-    );
-    anchor_lang::solana_program::program::invoke_signed(
-        &ix,
-        &[
-            ctx.accounts.program_id,
-            ctx.accounts.metadata,
-            ctx.accounts.current_authority,
-        ],
-        ctx.signer_seeds,
-    )
-    .map_err(Into::into)
+    let ix = todo!();
+    ix.invoke_signed(ctx.signer_seeds).map_err(Into::into)
 }
 
 #[derive(Accounts)]
-pub struct TokenMetadataUpdateAuthority<'info> {
-    pub program_id: AccountInfo<'info>,
-    pub metadata: AccountInfo<'info>,
-    pub current_authority: AccountInfo<'info>,
-    pub new_authority: AccountInfo<'info>,
+pub struct TokenMetadataUpdateAuthority {
+    pub program_id: AccountInfo,
+    pub metadata: AccountInfo,
+    pub current_authority: AccountInfo,
+    pub new_authority: AccountInfo,
 }
 
-pub fn token_metadata_update_field<'info>(
-    ctx: CpiContext<'_, '_, '_, 'info, TokenMetadataUpdateField<'info>>,
+pub fn token_metadata_update_field(
+    ctx: CpiContext<'_, '_, 'static, TokenMetadataUpdateField>,
     field: Field,
     value: String,
 ) -> Result<()> {
-    let ix = spl_token_metadata_interface::instruction::update_field(
-        ctx.accounts.program_id.key,
-        ctx.accounts.metadata.key,
-        ctx.accounts.update_authority.key,
-        field,
-        value,
-    );
-    anchor_lang::solana_program::program::invoke_signed(
-        &ix,
-        &[
-            ctx.accounts.program_id,
-            ctx.accounts.metadata,
-            ctx.accounts.update_authority,
-        ],
-        ctx.signer_seeds,
-    )
-    .map_err(Into::into)
+    let ix = todo!();
+    ix.invoke_signed(ctx.signer_seeds).map_err(Into::into)
 }
 
 #[derive(Accounts)]
-pub struct TokenMetadataUpdateField<'info> {
-    pub program_id: AccountInfo<'info>,
-    pub metadata: AccountInfo<'info>,
-    pub update_authority: AccountInfo<'info>,
+pub struct TokenMetadataUpdateField {
+    pub program_id: AccountInfo,
+    pub metadata: AccountInfo,
+    pub update_authority: AccountInfo,
 }
