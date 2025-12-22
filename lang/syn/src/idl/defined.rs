4c40149f4fe463028cc967709b31fcfc1cd65b11
Akash Thota <Otter-0x4ka5h@osec.io>
2025-12-22 22:11:02 +0530

    fix ci & fmt

--- lang/syn/src/idl/defined.rs
+++ lang/syn/src/idl/defined.rs
@@ -446,10 +446,7 @@ pub fn gen_idl_type(
                 _ => (),
             };
             let (inner, defined) = gen_idl_type(arg, generic_params)?;
-            Ok((
-                quote! { #idl::IdlType::Vec(Box::new(#inner)) },
-                defined,
-            ))
+            Ok((quote! { #idl::IdlType::Vec(Box::new(#inner)) }, defined))
         }
         syn::Type::Path(path) if the_only_segment_is(path, "Box") => {
             let segment = get_first_segment(path);
