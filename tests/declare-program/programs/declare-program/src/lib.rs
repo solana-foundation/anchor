5d3eade6a252484b26e20e90ca41282efde7d1b5
Tommy Johnson <tommyjohnson1028@gmail.com>
2026-01-12 15:59:38 -0500

    add compilation test

--- tests/declare-program/programs/declare-program/src/lib.rs
+++ tests/declare-program/programs/declare-program/src/lib.rs
@@ -12,6 +12,10 @@ declare_program!(external_legacy);
 // https://github.com/raydium-io/raydium-idl/blob/6123104304ebcb42be175cc297a2c221ac96bb96/raydium_clmm/amm_v3.json
 declare_program!(amm_v3);
 
+// Compilation check for optional accounts
+// https://github.com/solana-foundation/anchor/issues/4179
+declare_program!(optional);
+
 #[program]
 pub mod declare_program {
     use super::*;
