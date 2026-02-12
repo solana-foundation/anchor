c8fd63fbf0ece5f1b94250f3868b17014e302b85
Akash Thota <Otter-0x4ka5h@osec.io>
2026-02-12 22:30:58 +0530

    feat: added some token-22 extentions from latest push from anza

--- spl/src/token_2022_extensions/memo_transfer.rs
+++ spl/src/token_2022_extensions/memo_transfer.rs
@@ -5,13 +5,25 @@ use anchor_lang::pinocchio_runtime::pubkey::Pubkey;
 use anchor_lang::{context::CpiContext, Accounts};
 use anchor_lang::{Key, Result};
 
-pub fn memo_transfer_initialize(ctx: CpiContext<'_, '_, MemoTransfer>) -> Result<()> {
-    let ix = todo!();
+pub fn memo_transfer_enable(ctx: CpiContext<'_, '_, MemoTransfer>) -> Result<()> {
+    let signers: Vec<&AccountInfo> = ctx.remaining_accounts.iter().collect();
+    let ix = pinocchio_token_2022::instructions::memo_transfer::Enable {
+        token_account: &ctx.accounts.account,
+        authority: &ctx.accounts.authority,
+        signers: &signers,
+        token_program: &ctx.accounts.token_program_id.address(),
+    };
     ix.invoke_signed(ctx.signer_seeds).map_err(Into::into)
 }
 
 pub fn memo_transfer_disable(ctx: CpiContext<'_, '_, MemoTransfer>) -> Result<()> {
-    let ix = todo!();
+    let signers: Vec<&AccountInfo> = ctx.remaining_accounts.iter().collect();
+    let ix = pinocchio_token_2022::instructions::memo_transfer::Disable {
+        token_account: &ctx.accounts.account,
+        authority: &ctx.accounts.authority,
+        signers: &signers,
+        token_program: &ctx.accounts.token_program_id.address(),
+    };
     ix.invoke_signed(ctx.signer_seeds).map_err(Into::into)
 }
 
@@ -19,5 +31,5 @@ pub fn memo_transfer_disable(ctx: CpiContext<'_, '_, MemoTransfer>) -> Result<()
 pub struct MemoTransfer {
     pub token_program_id: AccountInfo,
     pub account: AccountInfo,
-    pub owner: AccountInfo,
+    pub authority: AccountInfo,
 }
