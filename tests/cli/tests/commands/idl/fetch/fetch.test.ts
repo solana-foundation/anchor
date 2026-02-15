a4d04a994219f36c2e26223790992d6f1ca0d8d1
Wilfred Almeida <60785452+WilfredAlmeida@users.noreply.github.com>
2026-02-15 05:36:53 +0000

    fix: idl fetch typescript tests
    Signed-off-by: GitHub <noreply@github.com>

--- tests/cli/tests/commands/idl/fetch/fetch.test.ts
+++ tests/cli/tests/commands/idl/fetch/fetch.test.ts
@@ -10,16 +10,11 @@ import {
 // const idlStdoutFile = path.join(__dirname, "idl-stdout");
 
 describe("idl fetch", () => {
-  it.skip("should succeed", () => {
+  it("should succeed", () => {
     const { testDir } = setupTest({ templateName: "default-test-program" });
     const workspaceName = "test-program";
     const workspaceDir = path.join(testDir, workspaceName);
-    // const outputPath = path.join(testDir, "cargo-calls");
-    // const tsnodePath = path.join(testDir, "ts-node");
-    const validatorCallsPath = path.join(testDir, "solana-test-validator-calls");
-    // const tsMochaCallsPath = path.join(testDir, "ts-mocha-calls");
     const rpcCallsPath = path.join(testDir, "rpc-calls");
-    // const programName = "test-program";
     const walletPath = path.join(testDir, "../../../../keypairs/aaLWzFHRPNhQwft1971qmPg2Q5eHwsHEWivqSkCDo9x.json")
 
     const rpcMock = path.join(MOCK_BIN_DIR, "rpc");
@@ -40,12 +35,7 @@ describe("idl fetch", () => {
       ],
       prependPath: [MOCK_BIN_DIR],
       env: {
-        // MOCK_CARGO_OUTPUT_PATH: outputPath,
-        // TS_NODE_OUTPUT_PATH: tsnodePath,
-        MOCK_SOLANA_TEST_VALIDATOR_OUTPUT_PATH: validatorCallsPath,
-        // MOCK_TS_MOCHA_OUTPUT_PATH: tsMochaCallsPath,
         MOCK_RPC_OUTPUT_PATH: rpcCallsPath,
-        // IDL_BUILD_STDOUT_FILE: idlStdoutFile,
       },
     });
 
