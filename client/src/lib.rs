20c061f4e11ef0063d460110d364c3dd523ee035
Akash Thota <Otter-0x4ka5h@osec.io>
2026-02-28 23:17:59 +0530

    feat(spl): added new missing instruciton in token & token-22 with newly added extension

--- client/src/lib.rs
+++ client/src/lib.rs
@@ -517,7 +517,7 @@ impl<'a, 'b, 'c, 'd> From<InstructionView<'a, 'b, 'c, 'd>>
     for InstructionViewWrapper<'a, 'b, 'c, 'd>
 {
     fn from(view: InstructionView<'a, 'b, 'c, 'd>) -> Self {
-        InstructionViewWrapper(view) 
+        InstructionViewWrapper(view)
     }
 }
 
