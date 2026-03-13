70385bba90b13cf097552f640645d2fb3a834c2a
acheron <acheroncrypto@gmail.com>
2026-03-13 03:11:56 +0100

    tests: Fix `errors`

--- tests/errors/test.sh
+++ tests/errors/test.sh
@@ -1,3 +1,3 @@
 # We don't want to build `multiple-errors` because it is expected to error
-anchor build -p errors --skip-lint
+anchor build -p errors --skip-lint --ignore-keys
 anchor test --skip-build
