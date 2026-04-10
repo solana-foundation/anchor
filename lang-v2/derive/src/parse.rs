1fb0a1df9123617f0eb20fa924d0d9b4d76c08c7
mixy1 <mixy1@ctf.mt>
2026-04-10 18:43:35 +0100

    refactor(v2): parse produces token streams, lib becomes assembler
    parse.rs now exports AccountField with pre-built load/constraints/exit
    token streams. All constraint desugaring (mut, signer, seeds with bump
    optimization, has_one, address, owner, constraint, namespaced, realloc,
    close with self-close prevention, init with AccountInitialize) moved
    from lib.rs to parse_field(). impl_accounts in lib.rs is now a pure
    assembler: collect fields, emit token streams. Zero constraint logic.

--- lang-v2/derive/src/parse.rs
+++ lang-v2/derive/src/parse.rs
@@ -1,4 +1,6 @@
 use syn::{ext::IdentExt, parse::{Parse, ParseStream}, Attribute, Expr, Ident, Token, Type};
+use quote::quote;
+use proc_macro2::TokenStream as TokenStream2;
 
 /// A namespaced constraint like `token::mint = expr`.
 pub struct NamespacedConstraint {
@@ -266,6 +268,326 @@ pub fn is_nested_type(ty: &Type) -> bool {
         }
     }
     false
+}
+
+pub struct AccountField {
+    pub name: Ident,
+    pub load: TokenStream2,
+    pub constraints: Vec<TokenStream2>,
+    pub exit: Option<TokenStream2>,
+    pub has_bump: bool,
+    // IDL metadata
+    pub idl_writable: bool,
+    pub idl_signer: bool,
+    pub idl_program_address: Option<String>,
+    pub idl_data_type: Option<TokenStream2>,
+}
+
+pub fn parse_field(field: &syn::Field) -> AccountField {
+    let field_name = field.ident.as_ref().expect("named field");
+    let field_ty = &field.ty;
+    let attrs = parse_account_attrs(&field.attrs);
+
+    let is_signer = field_ty_str(field_ty) == "Signer";
+    let is_init_signer = (attrs.is_init || attrs.is_init_if_needed) && attrs.seeds.is_none();
+    let program_address = extract_program_address(field_ty);
+    let idl_writable = attrs.is_mut;
+    let idl_signer = is_signer || is_init_signer;
+    let idl_data_type = extract_inner_data_type(field_ty);
+
+    let has_bump = attrs.seeds.is_some();
+
+    // --- Load ---
+    let load = if is_nested_type(field_ty) {
+        quote! { compile_error!("Nested<T> codegen not yet implemented"); }
+    } else if attrs.is_init {
+        let payer = attrs.payer.as_ref().expect("#[account(init)] requires payer");
+        let space = attrs.space.as_ref().expect("#[account(init)] requires space");
+        let inner_ty = extract_inner_type_for_init(field_ty)
+            .expect("#[account(init)] requires Account<T> or BorshAccount<T>");
+
+        let param_assignments: Vec<_> = attrs.namespaced.iter().map(|nc| {
+            let key = syn::Ident::new(&nc.key.to_lowercase(), proc_macro2::Span::call_site());
+            let value = &nc.value;
+            if nc.is_field_ref {
+                quote! { __p.#key = Some(#value.account()); }
+            } else {
+                quote! { __p.#key = Some(#value); }
+            }
+        }).collect();
+
+        let seeds_arg = if let Some(ref seeds) = attrs.seeds {
+            let seed_exprs = seeds;
+            quote! {
+                let (__pda, __bump) = anchor_lang_v2::find_program_address(
+                    &[#(#seed_exprs),*], __program_id,
+                );
+                if *__target.address() != __pda {
+                    return Err(anchor_lang_v2::ErrorCode::ConstraintSeeds.into());
+                }
+                __bumps.#field_name = __bump;
+                let __seeds: Option<&[&[u8]]> = Some(&[#(#seed_exprs),* , &[__bump]]);
+            }
+        } else {
+            quote! { let __seeds: Option<&[&[u8]]> = None; }
+        };
+
+        quote! {
+            let mut #field_name = {
+                let __target = __loader.next_view()?;
+                let __payer = #payer.account();
+                #seeds_arg
+                let __init_params = {
+                    type __P<'__a> = <#inner_ty as anchor_lang_v2::AccountInitialize>::Params<'__a>;
+                    let mut __p = <__P as Default>::default();
+                    #(#param_assignments)*
+                    __p
+                };
+                <#inner_ty as anchor_lang_v2::AccountInitialize>::create_and_initialize(
+                    __payer, &__target, #space, __program_id, &__init_params, __seeds,
+                )?;
+                <#field_ty as anchor_lang_v2::AnchorAccount>::load_mut(__target, __program_id)?
+            };
+        }
+    } else if attrs.is_init_if_needed {
+        let payer = attrs.payer.as_ref().expect("#[account(init_if_needed)] requires payer");
+        let space = attrs.space.as_ref().expect("#[account(init_if_needed)] requires space");
+        let inner_ty = extract_inner_type_for_init(field_ty)
+            .expect("#[account(init_if_needed)] requires Account<T> or BorshAccount<T>");
+
+        let seeds_arg = if let Some(ref seeds) = attrs.seeds {
+            let seed_exprs = seeds;
+            quote! {
+                let (__pda, __bump) = anchor_lang_v2::find_program_address(
+                    &[#(#seed_exprs),*], __program_id,
+                );
+                if *__target.address() != __pda {
+                    return Err(anchor_lang_v2::ErrorCode::ConstraintSeeds.into());
+                }
+                __bumps.#field_name = __bump;
+                let __seeds: Option<&[&[u8]]> = Some(&[#(#seed_exprs),* , &[__bump]]);
+            }
+        } else {
+            quote! { let __seeds: Option<&[&[u8]]> = None; }
+        };
+
+        let param_assignments: Vec<_> = attrs.namespaced.iter().map(|nc| {
+            let key = syn::Ident::new(&nc.key.to_lowercase(), proc_macro2::Span::call_site());
+            let value = &nc.value;
+            if nc.is_field_ref {
+                quote! { __p.#key = Some(#value.account()); }
+            } else {
+                quote! { __p.#key = Some(#value); }
+            }
+        }).collect();
+
+        quote! {
+            let mut #field_name = {
+                let __target = __loader.next_view()?;
+                let __already_init = __target.owned_by(__program_id) && __target.data_len() > 0;
+                if __already_init {
+                    <#field_ty as anchor_lang_v2::AnchorAccount>::load_mut(__target, __program_id)?
+                } else {
+                    let __payer = #payer.account();
+                    #seeds_arg
+                    let __init_params = {
+                        type __P<'__a> = <#inner_ty as anchor_lang_v2::AccountInitialize>::Params<'__a>;
+                        let mut __p = <__P as Default>::default();
+                        #(#param_assignments)*
+                        __p
+                    };
+                    <#inner_ty as anchor_lang_v2::AccountInitialize>::create_and_initialize(
+                        __payer, &__target, #space, __program_id, &__init_params, __seeds,
+                    )?;
+                    <#field_ty as anchor_lang_v2::AnchorAccount>::load_mut(__target, __program_id)?
+                }
+            };
+        }
+    } else if attrs.is_mut {
+        quote! {
+            let mut #field_name = __loader.next_mut::<#field_ty>()?;
+        }
+    } else {
+        quote! {
+            let #field_name = __loader.next::<#field_ty>()?;
+        }
+    };
+
+    // --- Constraints ---
+    let mut constraints = Vec::new();
+
+    // mut writability check
+    if attrs.is_mut && !attrs.is_init && !attrs.is_init_if_needed {
+        constraints.push(quote! {
+            if !#field_name.account().is_writable() {
+                return Err(anchor_lang_v2::ErrorCode::ConstraintMut.into());
+            }
+        });
+    }
+
+    // signer check
+    if attrs.is_signer {
+        constraints.push(quote! {
+            if !#field_name.account().is_signer() {
+                return Err(anchor_lang_v2::ErrorCode::ConstraintSigner.into());
+            }
+        });
+    }
+
+    // Seeds constraint (non-init, non-init_if_needed)
+    if !attrs.is_init && !attrs.is_init_if_needed {
+        if let Some(ref seeds) = attrs.seeds {
+            let seed_exprs = seeds;
+            if let Some(Some(ref bump_expr)) = attrs.bump {
+                constraints.push(quote! {
+                    {
+                        let __bump_val: u8 = #bump_expr;
+                        anchor_lang_v2::verify_program_address(
+                            &[#(#seed_exprs),* , &[__bump_val]],
+                            __program_id,
+                            #field_name.account().address(),
+                        )?;
+                        __bumps.#field_name = __bump_val;
+                    }
+                });
+            } else {
+                constraints.push(quote! {
+                    let (__pda, __bump) = anchor_lang_v2::find_program_address(
+                        &[#(#seed_exprs),*], __program_id,
+                    );
+                    if *#field_name.account().address() != __pda {
+                        return Err(anchor_lang_v2::ErrorCode::ConstraintSeeds.into());
+                    }
+                    __bumps.#field_name = __bump;
+                });
+            }
+        }
+    }
+
+    // has_one
+    for (ho, ho_err) in &attrs.has_one {
+        let err = if let Some(ref e) = ho_err {
+            quote! { core::convert::Into::into(#e) }
+        } else {
+            quote! { anchor_lang_v2::ErrorCode::ConstraintHasOne.into() }
+        };
+        constraints.push(quote! {
+            if AsRef::<[u8]>::as_ref(&#field_name.#ho) != AsRef::<[u8]>::as_ref(#ho.account().address()) {
+                return Err(#err);
+            }
+        });
+    }
+
+    // address
+    if let Some(ref addr) = attrs.address {
+        let err = if let Some(ref e) = attrs.address_error {
+            quote! { core::convert::Into::into(#e) }
+        } else {
+            quote! { anchor_lang_v2::ErrorCode::ConstraintAddress.into() }
+        };
+        constraints.push(quote! {
+            if *#field_name.account().address() != #addr {
+                return Err(#err);
+            }
+        });
+    }
+
+    // owner
+    if let Some(ref owner_expr) = attrs.owner {
+        let err = if let Some(ref e) = attrs.owner_error {
+            quote! { core::convert::Into::into(#e) }
+        } else {
+            quote! { anchor_lang_v2::ErrorCode::ConstraintOwner.into() }
+        };
+        constraints.push(quote! {
+            if !#field_name.account().owned_by(&#owner_expr) {
+                return Err(#err);
+            }
+        });
+    }
+
+    // constraint
+    if let Some(ref expr) = attrs.constraint {
+        let err = if let Some(ref custom_err) = attrs.constraint_error {
+            quote! { core::convert::Into::into(#custom_err) }
+        } else {
+            quote! { anchor_lang_v2::ErrorCode::ConstraintRaw.into() }
+        };
+        constraints.push(quote! {
+            if !(#expr) {
+                return Err(#err);
+            }
+        });
+    }
+
+    // namespaced constraints (skip for init/init_if_needed)
+    if !attrs.is_init && !attrs.is_init_if_needed {
+        for nc in &attrs.namespaced {
+            let ns = syn::Ident::new(&nc.namespace, proc_macro2::Span::call_site());
+            let key = syn::Ident::new(&nc.key, proc_macro2::Span::call_site());
+            let value = &nc.value;
+            constraints.push(quote! {
+                anchor_lang_v2::constraints::Constrain::<
+                    anchor_lang_v2::constraints::#ns::#key
+                >::constrain(
+                    &#field_name,
+                    AsRef::<anchor_lang_v2::Address>::as_ref(&#value),
+                )?;
+            });
+        }
+    }
+
+    // realloc
+    if let Some(ref new_space) = attrs.realloc {
+        let realloc_payer = attrs.realloc_payer.as_ref().expect("realloc requires realloc_payer");
+        let zero_fill = attrs.realloc_zero;
+        constraints.push(quote! {
+            {
+                let __new_space = #new_space;
+                let __info = #field_name.account();
+                let __current_len = __info.data_len();
+                if __new_space != __current_len {
+                    let mut __view = *__info;
+                    anchor_lang_v2::realloc_account(
+                        &mut __view,
+                        __new_space,
+                        #realloc_payer.account(),
+                        #zero_fill,
+                    )?;
+                }
+            }
+        });
+    }
+
+    // close (self-close prevention constraint + exit)
+    let exit = if let Some(ref close_target) = attrs.close {
+        constraints.push(quote! {
+            if #field_name.account().address() == #close_target.account().address() {
+                return Err(anchor_lang_v2::ErrorCode::ConstraintClose.into());
+            }
+        });
+        Some(quote! {
+            anchor_lang_v2::AnchorAccount::close(&mut self.#field_name, *self.#close_target.account())?;
+        })
+    } else if attrs.is_mut {
+        Some(quote! {
+            anchor_lang_v2::AnchorAccount::exit(&mut self.#field_name)?;
+        })
+    } else {
+        None
+    };
+
+    AccountField {
+        name: field_name.clone(),
+        load,
+        constraints,
+        exit,
+        has_bump,
+        idl_writable,
+        idl_signer,
+        idl_program_address: program_address,
+        idl_data_type,
+    }
 }
 
 /// Extract the well-known address from `Program<T>` types.
