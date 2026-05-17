90ed3509d8897e0020b4b649c54be357a54ca022
Robert Chen <me@robertchen.cc>
2026-05-17 10:30:11 -0400

    cli: include support info in version output

--- cli/src/bin/main.rs
+++ cli/src/bin/main.rs
@@ -1,5 +1,19 @@
 use {anchor_cli::Opts, anyhow::Result, clap::Parser};
 
 fn main() -> Result<()> {
+    if is_version_request() {
+        print!("{}", anchor_cli::support_version_report());
+        return Ok(());
+    }
+
     anchor_cli::entry(Opts::parse())
+}
+
+fn is_version_request() -> bool {
+    let mut args = std::env::args_os().skip(1);
+    let Some(arg) = args.next() else {
+        return false;
+    };
+
+    (arg == "--version" || arg == "-V") && args.next().is_none()
 }
